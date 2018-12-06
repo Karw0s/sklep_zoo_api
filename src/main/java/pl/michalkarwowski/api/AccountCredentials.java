d4aaadf4d17041d2632b12244aeae0a04ac6798d
Michal Karwowski <michal.karwowski.0@gmail.com>
2018-12-06 14:36:54 +0100

    tutorial securing-spring-boot-with-jwts

---
+++ src/main/java/pl/michalkarwowski/api/AccountCredentials.java
@@ -0,0 +1,10 @@
+package pl.michalkarwowski.api;
+
+import lombok.Data;
+
+@Data
+public class AccountCredentials {
+    private String username;
+    private String password;
+
+}
--- src/main/java/pl/michalkarwowski/api/SecurityConfiguration.java
+++ src/main/java/pl/michalkarwowski/api/SecurityConfiguration.java
@@ -1,23 +1,42 @@
 package pl.michalkarwowski.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import pl.michalkarwowski.api.model.User;
-import pl.michalkarwowski.api.service.DetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                // We filter the api/login requests
+                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                // And filter other requests to check the presence of JWT in header
+                .addFilterBefore(new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Create a default account
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("password")
+                .roles("ADMIN");
+    }
+
+    /*@Autowired
     DetailsService detailsService;
 
     @Override
@@ -33,7 +52,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .csrf().disable();
-    }
+    }*/
 
     /*@Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
